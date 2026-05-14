26b77071708aa6884ef0fb01edaec76f4750774a
Jaime Chile <jaime.chile@gci.gt>
2026-05-14 14:55:30 -0600

    Fix: eliminar modo libreria para despliegue en vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,34 +3,15 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
- server: {
+  server: {
     host: true,
     port: 5174,
     allowedHosts: true
-
   },
-
-
   plugins: [react()],
   build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
-      name: 'CocinasCenterInicio',
-      fileName: (format) => {
-        if (format === 'es') return 'index.es.js';
-        if (format === 'umd') return 'index.umd.js';
-        return 'index.js';
-      }
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM'
-        }
-      }
-    },
+    // SE ELIMINÓ 'lib' para que compile el index.html principal
+    // SE ELIMINÓ 'external' para que Vercel incluya React y React-DOM en la web
     sourcemap: true,
     minify: 'terser'
   },
